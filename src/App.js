398f28cfe945a9b5068bb49fd7b87dcc98ad344e
choijunghun <chjjh0@gmail.com>
2021-01-28 00:18:01 +0900

    초기 환경세팅

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  console.log(isOpen);
+
+  return <>FEBASE BUTTON</>;
+}
+
+export default App;
