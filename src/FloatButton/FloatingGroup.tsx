698e799ef24cc01019299c8d2e63c8c641ebe61f
jinsunee <jsunee.dev@gmail.com>
2021-03-08 00:35:15 +0900

    update - 기존 코드 수정

--- src/FloatButton/FloatingGroup.tsx
+++ src/FloatButton/FloatingGroup.tsx
@@ -1,5 +1,5 @@
 import { Direction, Size } from '../types';
-import React, {CSSProperties, ReactElement, useEffect, useState} from 'react';
+import React, {CSSProperties, ReactElement, useState} from 'react';
 
 import { Add } from '@emotion-icons/remix-line';
 import styled from '@emotion/styled';
@@ -36,38 +36,29 @@ function FloatingGroup(props: Props): ReactElement {
     <Add size="70%" color="#397CCA" />
   );
 
-  const closeButtonHandler = (isSpread: boolean) => {
-    if(isSpread) toggleSpread(false);
-    return null;
+  const handleSpread = () => {
+    toggleSpread(prev => !prev);
   }
 
   return (
     <Container style={containerStyle} direction={direction}>
-      <RootButton style={rootButtonContainerStyle} size={size} spacing={spacing / 2}>
-        <RootButtonWrapper onMouseEnter={() => toggleSpread(true)} onClick={() => closeButtonHandler(isSpread)} isSpread={isSpread}>{renderRootButtonElement}</RootButtonWrapper>
+      <RootButton onClick={handleSpread} style={rootButtonContainerStyle} size={size} spacing={spacing / 2}>
+        <RootButtonWrapper 
+          isSpread={isSpread}
+        >
+          {renderRootButtonElement}
+        </RootButtonWrapper>
       </RootButton>
       {renderChildren}
     </Container>
   );
 }
 
-interface ContainerStyledType {
+type ContainerStyleProps = {
   direction: Direction;
 }
 
-interface ButtonContainer {
-  size: Size;
-  spacing: number;
-  direction: Direction;
-  order: number;
-  isSpread: boolean;
-}
-
-interface ButtonWrapperContainer {
-  isSpread: boolean;
-}
-
-const Container = styled.ul<ContainerStyledType>`
+const Container = styled.ul<ContainerStyleProps>`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -77,40 +68,56 @@ const Container = styled.ul<ContainerStyledType>`
   position: relative;
 `;
 
-// @ts-ignore
-const RootButton = styled((props) => (
-  <li style={{zIndex: 100}}>
-    <button {...props}></button>
-  </li>
-))<ButtonContainer>`
+interface ButtonContainer {
+  size: Size;
+  spacing: number;
+}
+
+const RootButton = styled.button<ButtonContainer>`
+  z-index: 100;
+  margin: ${({ spacing }) => spacing}px;
+  width: ${({ size }) => size}px;
+  height: ${({ size }) => size}px;
   border: none;
-  width: ${({ size }) => `${size}`}px;
-  height: ${({ size }) => `${size}`}px;
+  border-radius: 50px;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: #fff;
-  border-radius: 50%;
   cursor: pointer;
-  margin: ${({ spacing }) => spacing}px;
 
   &:focus {
     outline: none;
   }
+
+  &:hover {
+    opacity: 0.6;
+  }
 `;
 
+interface ButtonWrapperContainer {
+  isSpread: boolean;
+}
+
 const RootButtonWrapper = styled.div<ButtonWrapperContainer>`
-    -webkit-transition: -webkit-transform 0.3s ease-in-out;
-    -ms-transition: -ms-transform 0.3s ease-in-out;
-    transition: transform 0.3s ease-in-out;
     z-index: 100;
     position: relative;
+    
+    transition: transform 0.3s ease-in-out;
+    -ms-transition: -ms-transform 0.3s ease-in-out;
+    -webkit-transition: -webkit-transform 0.3s ease-in-out;
     transform: ${({isSpread}) => (isSpread ? 'rotate(135deg)' : 'none')};
     -ms-transform: ${({isSpread}) => (isSpread ? 'rotate(135deg)' : 'none')};
     -webkit-transform: ${({isSpread}) => (isSpread ? 'rotate(135deg)' : 'none')};
 `;
 
-const ItemButton = styled.li<ButtonContainer>`
+type ItemButtonStyleProps = {
+  direction: Direction;
+  order: number;
+  isSpread: boolean;
+} & ButtonContainer;
+
+const ItemButton = styled.li<ItemButtonStyleProps>`
   width: ${({ size }) => `${size}`}px;
   height: ${({ size }) => `${size}`}px;
   margin: ${({ spacing }) => spacing}px;
@@ -120,7 +127,8 @@ const ItemButton = styled.li<ButtonContainer>`
   z-index: ${({order}) => 100 - order};
   top: ${({direction, isSpread, order}) => (((direction === Direction.BOTTOM) && isSpread) ? order * 80 : ((direction === Direction.TOP) && isSpread ? order * (-80) : 0))}px;
   right: ${({direction, isSpread, order}) => (((direction === Direction.LEFT) && isSpread) ? order * 80 : (((direction === Direction.RIGHT) && isSpread) ? order * (-80) : 0))}px;
-  transition: all 1s ease 0s;
+  transition: all 0.4s ease 0s;
+  opacity: ${({isSpread}) => isSpread ? 1 : 0};
 `;
 
 export default FloatingGroup;
